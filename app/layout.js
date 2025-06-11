c385ebe624210b1c0d71b2a8e02beb28299060e6
“[Amit <[amitk25783@gmail.com]>
2025-06-11 22:34:19 +0530

    BudgetBuddy.AI - Your AI Financial Buddy

---
+++ app/layout.js
@@ -0,0 +1,46 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "BadgetBuddy.AI",
+  keywords: [
+    "budgeting",
+    "finance",
+    "personal finance",
+    "expense tracking",
+    "income tracking",
+    "financial planning",
+    "AI finance",
+    "budget app",
+  ],
+  authors: [{ name: "Amit Kumar", url: "https://www.linkedin.com/in/amit-kumar-686196225/" }],
+  description: "One stop Finance Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with 💗 by Amit Kumar</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
